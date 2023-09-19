fe3965603233b52fe405d712a50786ed763b6a8d
Olga Ryzhova <120307340+Olga-Ryzhova@users.noreply.github.com>
2023-09-19 19:43:54 +0400

    First commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// 1. Стрелки в слайдере
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
@@ -15,14 +16,14 @@ $(document).ready(function(){
         ]
     });
 
-  // табы
+// 2. табы
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
       
-// реализация кнопок ПОДРОБНЕЕ и НАЗАД
+//3. реализация кнопок ПОДРОБНЕЕ и НАЗАД
     //   $('.catalog-item__link').each(function(i) {
     //     $(this).on('click', function(e) {
     //         e.preventDefault();
@@ -53,6 +54,111 @@ $(document).ready(function(){
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
 
+ // 4. модальные окна
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
 
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {  
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+// 5. Валидация форм
+    // $('#consultation-form').validate();
+    // $('#consultation form').validate({
+    //     rules: {
+    //         name: "required",
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: "Пожалуйста, введите свое имя",
+    //         phone: "Пожалуйста, введите свой номер телефона",
+    //         email: {
+    //           required: "Пожалуйста, введите свою почту",
+    //           email: "Неправильно введен адрес почты"
+    //         }
+    //       }
+        
+    // });
+    // $('#order form').validate();
+// Оптимизация кода
+    function valideForms (form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        })
+    };
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+// 6. Маска номера телефона
+    $('input[name=phone]').mask("+7(999) 999-99-99");
+
+// 7. Отправка писем
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+// 8. Элемент "вверх" плавный скролл
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    
+// 9. Плавный скролл
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+// 10. Анимация с отзывами
+    new WOW().init();
 
 });
